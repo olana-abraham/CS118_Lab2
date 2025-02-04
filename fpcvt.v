5768ffdf0d7aba09d8eb5b8125ca6b9ad44aa933
dude2485 <49ersfandude1000@gmail.com>
2025-02-04 13:50:09 -0800

    v1 of rounding module

--- fpcvt.v
+++ fpcvt.v
@@ -17,6 +17,31 @@ module FloatingPointConvert;
 
 endmodule
 
-module Rounding;
+module Rounding(
+    input wire [3:0] exponent,
+          wire fifth_bit,
+          wire [4:0] significand
+    output wire [2:0] E,
+           wire [3:0] F;
+);
+
+    if(fifth_bit == 1) begin
+
+        significand = significand + 1;
+
+        if(significand[4] == 1) begin
+        
+            exponent = exponent + 1;
+            significand = significand >> 1;
+
+            if(exponent[3] == 1) begin
+                exponent = 'b111;
+                significand = 'b1111;
+            end
+        end
+    end
+
+    assign E = exponent[2:0];
+    assign F = significand[3:0];
 
 endmodule
