51d2dfbe88c4b6c14bcfee5e8c53e09d67473268
dude2485 <49ersfandude1000@gmail.com>
2025-02-04 13:17:01 -0800

    wire d width change.

--- fpcvt.v
+++ fpcvt.v
@@ -1,17 +1,15 @@
-module TwosToSignMag (
+module TwosToSignMag ( 
     input wire [11:0] d,
-    output wire [10:0] absoluteNum,
+    output wire [11:0] absoluteNum,
     output wire sign);
 
     assign sign = d[11]
     if (sign == 1) begin
-        assign absoluteNum = ~d[10:0] + 1
+        assign absoluteNum = ~d[11:0] + 1
     end
     else begin
-        assign absoluteNum = d[10:0]
+        assign absoluteNum = d[11:0]
     end
-
-
 
 endmodule
 
