c23d2f12030b43fa5b4c38b3d0dffe21f0b5d29e
olana-abraham <olana.abraham@hgs.hiddengeniusproject.org>
2025-02-05 21:36:36 -0800

    floatingpoint

--- fpcvt.v
+++ fpcvt.v
@@ -13,7 +13,48 @@ module TwosToSignMag (
 
 endmodule
 
-module FloatingPointConvert;
+module FloatingPointConvert (
+    input wire [11:0] absoluteNum,
+    output reg [2:0] exponent,
+    output reg [3:0] significand
+);
+
+    always @(*) begin
+
+        exponent = 0;
+        significand = absoluteNum[3:0];
+
+        if (absoluteNum[10]) begin
+            exponent = 7;
+            significand = absoluteNum[9:6];
+        end
+        else if (absoluteNum[9]) begin
+            exponent = 6;
+            significand = absoluteNum[8:5];
+        end
+        else if (absoluteNum[8]) begin
+            exponent = 5;
+            significand = absoluteNum[7:4];
+        end
+        else if (absoluteNum[7]) begin
+            exponent = 4;
+            significand = absoluteNum[6:3];
+        end
+        else if (absoluteNum[6]) begin
+            exponent = 3;
+            significand = absoluteNum[5:2];
+        end
+        else if (absoluteNum[5]) begin
+            exponent = 2;
+            significand = absoluteNum[4:1];
+        end
+        else if (absoluteNum[4]) begin
+            exponent = 1;
+            significand = absoluteNum[3:0];
+        end
+    end
+
+
 
 endmodule
 
